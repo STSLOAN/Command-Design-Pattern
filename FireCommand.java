b2d74c11ae7d2e3e9377f67e907da07e9995e118
ConnorShipm1 <77517020+ConnorShipm1@users.noreply.github.com>
2021-02-27 10:55:32 -0500

    Update FireCommand.java

--- FireCommand.java
+++ FireCommand.java
@@ -2,10 +2,10 @@ public class FireCommand implements Command{
     private Robot robot;
     
     public FireCommand(Robot robot) {
-
+        this.robot = robot;
     }
 
     public void execute() {
-
+        robot.fire;
     }
 }
