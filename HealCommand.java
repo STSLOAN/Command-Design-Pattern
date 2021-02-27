dac7cb1fc6c565a20b5b95a921c0eff8c9d6add3
ConnorShipm1 <77517020+ConnorShipm1@users.noreply.github.com>
2021-02-27 11:03:35 -0500

    Update HealCommand.java

--- HealCommand.java
+++ HealCommand.java
@@ -1,11 +1,21 @@
+/**
+ * main method of the class. implements the Command interface.
+ */
 public class HealCommand implements Command{
     private Robot robot;
     
+    /**
+     * The HealCommand sets up the robot
+     * @param robot that is used
+     */
     public HealCommand(Robot robot) {
-
+        this.robot = robot;
     }
 
+    /**
+     * Executes the method and calls heal() from Robot class
+     */
     public void execute() {
-
+        robot.heal();
     }
 }
