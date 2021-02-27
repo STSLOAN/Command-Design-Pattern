9f899f9e887dafb57d6458aa5312af040e32ca8c
ConnorShipm1 <77517020+ConnorShipm1@users.noreply.github.com>
2021-02-27 11:03:31 -0500

    Update FireCommand.java

--- FireCommand.java
+++ FireCommand.java
@@ -1,11 +1,21 @@
+/**
+ * main method of the class. implements the Command interface.
+ */
 public class FireCommand implements Command{
     private Robot robot;
     
+    /**
+     * The FireCommand sets up the robot
+     * @param robot that is used
+     */
     public FireCommand(Robot robot) {
         this.robot = robot;
     }
 
+    /**
+     * Executes the method and calls fire() from Robot class
+     */
     public void execute() {
-        robot.fire;
+        robot.fire();
     }
 }
