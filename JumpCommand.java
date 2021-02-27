98719b552c090c9f1d7bfa46bccd0afa334ce08b
ConnorShipm1 <77517020+ConnorShipm1@users.noreply.github.com>
2021-02-27 11:03:37 -0500

    Update JumpCommand.java

--- JumpCommand.java
+++ JumpCommand.java
@@ -1,11 +1,21 @@
+/**
+ * main method of the class. implements the Command interface.
+ */
 public class JumpCommand implements Command{
     private Robot robot;
 
+    /**
+     * The JumpCommand sets up the robot
+     * @param robot that is used
+     */
     public JumpCommand(Robot robot) {
-
+        this.robot = robot;
     }
-
+    
+    /**
+     * Executes the method and calls jump() from Robot class
+     */
     public void execute() {
-
+        robot.jump();
     }
 }
