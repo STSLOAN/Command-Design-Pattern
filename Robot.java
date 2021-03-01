514c1a4199aae49ab637095a58dfa0d1d41901c8
GurdisWord <70558499+GurdisWord@users.noreply.github.com>
2021-03-01 18:41:32 -0500

    initial commit

--- Robot.java
+++ Robot.java
@@ -1,25 +1,31 @@
-/**
- * main method of the class
- */
 public class Robot {
     
     public Robot(String name) {
-
+    	this.name = name;
+    	System.out.println("Welcome, my name is "+name+", I'm a friendly robot");
     }
 
     public void pickup() {
-
+    	System.out.println("Oh treasure, picking it up!");
+    	
+    	return;
     }
 
     public void jump() {
-
+    	System.out.println("Oh no a hazard, I'm jumping over it.");
+    	
+    	return;
     }
 
     public void fire() {
-
+    	System.out.println("Bad guys! Fire my blow torch at them.");
+    	
+    	return;
     }
 
     public void heal() {
+    	System.out.println("Thanks for healing my wounds.");
         
+    	return;
     }
 }
